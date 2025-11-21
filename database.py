4aaf6b48f9f26656f75dae1087d8b02ab0d69bfe
Theo004-coder <roma1972mar@gmail.com>
2025-11-21 23:20:15 +0400

    full path to db
    add full path in create engine

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
  
-engine = create_engine('sqlite:///C:\\students.db', echo=False)
+engine = create_engine('sqlite:///C:\Users\rom17\Documents\GitHub\Volunteers-telegram-bot-/users.db', echo=False)
 Base = declarative_base()
  
